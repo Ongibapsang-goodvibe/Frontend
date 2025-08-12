2dd5e0dcdc752ed438ba75721b4d62c4fa1e4b12
yxpjseo <yjins2004@gmail.com>
2025-08-12 15:52:04 +0900

    app.jsx 설정, 기본 스타일 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-export default function App() {
-  return <h1>온기밥상</h1>
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReceiptCheck from "./pages/delivery-feedback/receipt-check";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/receipt-check" element={<ReceiptCheck />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;

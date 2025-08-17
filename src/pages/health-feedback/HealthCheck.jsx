b2cddc211ee4f5b0615baedcad6141e92763212d
yxpjseo <yjins2004@gmail.com>
2025-08-17 22:41:44 +0900

    HealthCheck.jsx 수정

--- src/pages/health-feedback/HealthCheck.jsx
+++ src/pages/health-feedback/HealthCheck.jsx
@@ -25,6 +25,7 @@ const HealthCheck = () => {
                     onClick={() => setSelected('yes')}
                 >
                     <span>아무 문제 없어요</span>
+                    <img src="blue.svg"/>
                 </Button>
                 <Button
                     type="button"
@@ -32,6 +33,7 @@ const HealthCheck = () => {
                     onClick={() => setSelected('no')}
                 >
                     <span>어딘가 불편해요</span>
+                    <img src="red.svg"/>
                 </Button>
 
                 <Button

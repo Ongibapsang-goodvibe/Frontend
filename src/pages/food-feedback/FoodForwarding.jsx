5c7dec0bcde59382efb513eafce42a1fb131e420
yxpjseo <yjins2004@gmail.com>
2025-08-17 22:22:53 +0900

    food-feedback 디자인 변경사항 반영

--- src/pages/food-feedback/FoodForwarding.jsx
+++ src/pages/food-feedback/FoodForwarding.jsx
@@ -8,7 +8,8 @@ const FoodForwarding = () => {
     return(
         <>
             <Wrapper>
-                <h1>남겨주신 답변을<br />후기로 남겼어요.</h1>
+                <h1>남겨주신 답변을<br />
+                <span>후기</span>로 남겼어요.</h1>
                 <img src="HappyImage.png" alt="행복한 너구리" />
                 <Button
                     type="button"
@@ -25,16 +26,22 @@ const FoodForwarding = () => {
 export default FoodForwarding;
 
 const Wrapper = styled.div`
+  width:100%;
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  background: linear-gradient(180deg, #FFEECF 0%, #FFDDB2 100%);
 
   h1{
     margin-top: 5.88rem;
-    color: #fff;
+    color: var(--background-color);
     font-size: 2.5rem;
     font-weight: 700;
+  }
+
+  span{
+    color: #E75900;
   }
 `;
 
@@ -47,8 +54,13 @@ const Button = styled.button`
 
   cursor: pointer;
 
+  span{
+    color: #000;
+  }
+
   &.health-check{
   border-radius: 0.75rem;
+  border: 0;
   height: 4.5rem;
 
   background: var(--main-color);

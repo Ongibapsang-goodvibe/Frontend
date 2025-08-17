445cc900166f9bb77af23458e6b7dbcf67f67014
yxpjseo <yjins2004@gmail.com>
2025-08-17 22:53:18 +0900

    health-feedback 디자인 변경사항 반영

--- src/pages/health-feedback/HealthForwarding.jsx
+++ src/pages/health-feedback/HealthForwarding.jsx
@@ -6,8 +6,10 @@ const HealthForwarding = () => {
     return(
         <>
             <Wrapper>
-                <h1>모든 답변이 끝났어요</h1>
-                <h2>다음에 또 뵐게요!</h2>
+                <h1><span>모든</span> 답변이 끝났어요</h1>
+                <h2>답변이 보고서에 반영되었어요.</h2>
+                <p>*보호자 계정과 연결되어 있는 경우 <br />
+                보호자용 보고서에도 반영</p>
                 <img src="LovelyImage.png" alt="하트뿅뿅 너구리" />
             </Wrapper>
         </>
@@ -17,22 +19,39 @@ const HealthForwarding = () => {
 export default HealthForwarding;
 
 const Wrapper = styled.div`
+  width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  background: linear-gradient(180deg, #FFEECF 0%, #FFDDB2 100%);
 
   h1{
-    margin-top: 6.25rem;
-    color: #fff;
+    margin-top: 3.37rem;
+    color: var(--background-color);
     font-size: 2.5rem;
     font-weight: 700;
   }
 
+  span{
+    color: #E75900;
+  }
+
   h2{
-    margin-bottom: 2.88rem;
-    color: #FFF;
-    font-size: 2.5rem;
+    margin-bottom: 0.37rem;
+    color: var(--background-color);
+    font-size: 1.5rem;
     font-weight: 500;
+  }
+
+  p{
+    margin-top: 3.31rem;
+    color: rgba(0, 0, 0, 0.35);
+    font-size: 1.25rem;
+    font-weight: 500;
+  }
+
+  img{
+    margin-top: 1.19rem;
   }
 `;

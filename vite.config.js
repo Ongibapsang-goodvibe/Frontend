b80e9242a44eca12d8f16298db6579fa1eaffd43
yxpjseo <yjins2004@gmail.com>
2025-08-18 15:52:12 +0900

    MenuVoice api연동

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/restaurants/api": {
+        target: "http://localhost:8000", // Django 등 백엔드
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
